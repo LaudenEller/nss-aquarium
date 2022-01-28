da16a12ce715dc94e883f08686293221a2bcbc75
Lauden Eller <laudlauden@gmail.com>
2022-01-28 13:45:03 -0800

    got things loaded but still looks like a mess

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,33 @@
-import { getFish, getMartinsTips, getHolyFish, getSoldierFish, getRegularFish, getLocation, } from './databse.js'
+import { getFish, getMartinsTips, getHolyFish, getSoldierFish, getRegularFish, getLocation, getMaintenanceInstructions, } from './databse.js'
 
 // Import the FishList function from the correct module
-import { holyFishList, soldierFishList, regularFishList, locationList, martinsTipsList } from './FishList.js'
+import { holyFishList, soldierFishList, regularFishList, locationList, martinsTipsList, maintenanceInstructionsList } from './FishList.js'
 
-// const getFishVar = getFish()
-// const holyFish = getHolyFish(getFishVar)
-
-//     console.log(holyFish)
-
-// const soldierFish = getSoldierFish(getFishVar)
-
-//     console.log(soldierFish)
-
-// const regularFish = getRegularFish(getFishVar)
-
-//     console.log(regularFish)
-
-
-/*
-    What is the CSS selector for the element where you
-    want to display the fish?
-
-    Use . for elements with a "class" attribute
-    Use # for elements with an "id" attribute
- */
 const holyFishParentHTMLElement = document.querySelector(".holy-fish-container")
 
 holyFishParentHTMLElement.innerHTML = holyFishList()
 
+
 const soldierFishParentHTMLElement = document.querySelector(".soldier-fish-container")
 
-
 soldierFishParentHTMLElement.innerHTML = soldierFishList()
+
 
 const regularFishParentHTMLElement = document.querySelector(".regular-fish-container")
 
 regularFishParentHTMLElement.innerHTML = regularFishList()
 
 
-// print locations to location column
-const allLocations = getLocation()
-
-for (const location of allLocations) {
-    console.log(location)
-}
-
-const locationParentHTMLElement = document.querySelector(".location-column")
-
+const locationParentHTMLElement = document.querySelector(".location-container")
 
 locationParentHTMLElement.innerHTML = locationList()
 
 
-
-// print martins tips to maintenance column
-const allMartinsTips = getMartinsTips()
-
-for (const martinsTips of allMartinsTips) {
-    console.log(martinsTips)
-}
-
-const maintenanceParentHTMLElement = document.querySelector(".maintenance-column")
-
+const maintenanceParentHTMLElement = document.querySelector(".tipsList")
 
 maintenanceParentHTMLElement.innerHTML = martinsTipsList()
+
+
+const instructionsParentHTMLElement = document.querySelector(".tank-maintenance-info")
+
+instructionsParentHTMLElement.innerHTML = maintenanceInstructionsList()
